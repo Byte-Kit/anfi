b0a32380e581b09a541df80819255e8d3e5211c4
Byte <byte.tpht@gmail.com>
2026-06-02 22:39:41 +0700

    chore(db|stub): add usage example

--- src/db/stub.ts
+++ src/db/stub.ts
@@ -2,6 +2,19 @@ import { stub } from "@std/testing/mock";
 import { DbConnection } from "./common.ts";
 import { ConnectionBuilder } from "./connection.ts";
 
+/**
+ * Stubs the database connection for testing purposes.
+ * @example
+ * ```ts
+ * beforeEach(() => {
+ *  stubDbConnection();
+ * })
+ *
+ * it("bar should foo", () => {
+ *  const stubConnection = new ConnectionBuilder().get();
+ * })
+ * ```
+ */
 export function stubDbConnection() {
   stub(
     ConnectionBuilder.prototype,
